a89551e237ccb4798b4678054b56830ed9cea9a3
chenlianghong <clh021@gmail.com>
2022-07-20 07:31:58 +0800

    svcstringstudy: make endpoint

--- svcstringstudy/endpoint.go
+++ svcstringstudy/endpoint.go
@@ -1,3 +1,28 @@
 package main
 
+import (
+	"context"
+
+	"github.com/go-kit/kit/endpoint"
+)
+
 // type Endpoint func(ctx context.Context, request interface{}) (response interface{}, err error)
+
+func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(uppercaseRequest)
+		v, err := svc.Uppercase(req.S)
+		if err != nil {
+			return uppercaseResponse{v, err.Error()}, nil
+		}
+		return uppercaseResponse{v, ""}, nil
+	}
+}
+
+func makeCountEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(countRequest)
+		v := svc.Count(req.S)
+		return countResponse{v}, nil
+	}
+}
